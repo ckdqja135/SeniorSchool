b0bd9fcf095262e814b562c4c567f7ffb5bfa62d
ckdqja135 <ckdqja135@naver.com>
2025-01-29 22:22:46 +0900

    model명 수정

--- Backend/model/univcomment.js
+++ Backend/model/univcomment.js
@@ -2,55 +2,52 @@ const Sequelize = require('sequelize');
 
 module.exports = class UnivComment extends Sequelize.Model {
     static init(sequelize) {
-        return super.init(
-            {
-                CommentId: {
-                    type: Sequelize.BIGINT,
-                    primaryKey: true,
-                    autoIncrement: true,
-                    allowNull: false,
-                },
-                BoardNo: {
-                    type: Sequelize.BIGINT,
-                    allowNull: false,
-                },
-                CommentLike: {
-                    type: Sequelize.BIGINT,
-                    allowNull: false,
-                    defaultValue: 0,
-                },
-                CommentDepth: {
-                    type: Sequelize.BIGINT,
-                    allowNull: true,
-                },
-                WriterId: {
-                    type: Sequelize.STRING(45),
-                    allowNull: false,
-                },
-                WriterPw: {
-                    type: Sequelize.STRING(100),
-                    allowNull: false,
-                },
-                CommnetPerent: {
-                    type: Sequelize.BIGINT,
-                    allowNull: true,
-                },
-                CommentContent: {
-                    type: Sequelize.STRING(200),
-                    allowNull: false,
-                },
+        return super.init({
+            CommentId: {
+                type: Sequelize.BIGINT,
+                primaryKey: true,
+                autoIncrement: true,
+                allowNull: false,
             },
-            {
-                sequelize,
-                modelName: 'UnivComment',
-                tableName: 'univcomment',
-                charset: 'utf8',
-                timestamps: false,
-            }
-        );
+            BoardNo: {
+                type: Sequelize.BIGINT,
+                allowNull: false,
+            },
+            CommentLike: {
+                type: Sequelize.BIGINT,
+                allowNull: false,
+                defaultValue: 0,
+            },
+            CommentDepth: {
+                type: Sequelize.BIGINT,
+                allowNull: true,
+            },
+            WriterId: {
+                type: Sequelize.STRING(45),
+                allowNull: false,
+            },
+            WriterPw: {
+                type: Sequelize.STRING(100),
+                allowNull: false,
+            },
+            CommnetPerent: {
+                type: Sequelize.BIGINT,
+                allowNull: true,
+            },
+            CommentContent: {
+                type: Sequelize.STRING(200),
+                allowNull: false,
+            },
+        }, {
+            sequelize,
+            modelName: "UnivComment",
+            tableName: "tb_univcomment",
+            charset: "utf8",
+            timestamps: false,
+        });
     }
 
     static associate(db) {
-        // Add associations if there are any
+        // db.UnivComment.belongsTo(db.UnivBoard, { foreignKey: "BoardNo", targetKey: "BoardNo" });
     }
 };
--- Backend/model/universityinfo.js
+++ Backend/model/universityinfo.js
@@ -2,75 +2,75 @@ const Sequelize = require('sequelize');
 
 module.exports = class UniversityInfo extends Sequelize.Model {
     static init(sequelize) {
-        return super.init(
-            {
-                UnivNo: {
-                    type: Sequelize.INTEGER,
-                    primaryKey: true,
-                    autoIncrement: true,
-                    allowNull: false,
-                },
-                UnivName: {
-                    type: Sequelize.STRING(60),
-                    allowNull: false,
-                },
-                UnivLocate: {
-                    type: Sequelize.STRING(45),
-                    allowNull: false,
-                },
-                UnivType: {
-                    type: Sequelize.STRING(45),
-                    allowNull: false,
-                },
-                UnivEstablish: {
-                    type: Sequelize.STRING(45),
-                    allowNull: false,
-                },
-                UnivPresident: {
-                    type: Sequelize.STRING(45),
-                    allowNull: false,
-                },
-                UnivCampos: {
-                    type: Sequelize.STRING(45),
-                    allowNull: false,
-                },
-                UnivLateX: {
-                    type: Sequelize.DOUBLE,
-                    allowNull: false,
-                },
-                UnivLateY: {
-                    type: Sequelize.DOUBLE,
-                    allowNull: false,
-                },
-                UnivPageURL: {
-                    type: Sequelize.STRING(200),
-                    allowNull: false,
-                },
-                UnivIMG: {
-                    type: Sequelize.STRING(200),
-                    allowNull: false,
-                },
-                UnivLotAddr: {
-                    type: Sequelize.STRING(200),
-                    allowNull: false,
-                },
-                UnivAddr: {
-                    type: Sequelize.STRING(200),
-                    allowNull: false,
-                },
-                UnivMapIMG: {
-                    type: Sequelize.STRING(200),
-                    allowNull: false,
-                },
+        return super.init({
+            UnivNo: {
+                type: Sequelize.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+                allowNull: false,
             },
-            {
-                sequelize,
-                modelName: 'UniversityInfo',
-                tableName: 'universityinfo',
-                charset: 'utf8',
-                timestamps: false,
-            }
-        );
+            UnivName: {
+                type: Sequelize.STRING(60),
+                allowNull: false,
+            },
+            UnivLocate: {
+                type: Sequelize.STRING(45),
+                allowNull: false,
+            },
+            UnivType: {
+                type: Sequelize.STRING(45),
+                allowNull: false,
+            },
+            UnivEstablish: {
+                type: Sequelize.STRING(45),
+                allowNull: false,
+            },
+            UnivPresident: {
+                type: Sequelize.STRING(45),
+                allowNull: false,
+            },
+            UnivCampos: {
+                type: Sequelize.STRING(45),
+                allowNull: false,
+            },
+            UnivLateX: {
+                type: Sequelize.DOUBLE,
+                allowNull: false,
+            },
+            UnivLateY: {
+                type: Sequelize.DOUBLE,
+                allowNull: false,
+            },
+            UnivPageURL: {
+                type: Sequelize.STRING(200),
+                allowNull: false,
+            },
+            UnivIMG: {
+                type: Sequelize.STRING(200),
+                allowNull: false,
+            },
+            UnivLotAddr: {
+                type: Sequelize.STRING(200),
+                allowNull: false,
+            },
+            UnivAddr: {
+                type: Sequelize.STRING(200),
+                allowNull: false,
+            },
+            UnivMapIMG: {
+                type: Sequelize.STRING(200),
+                allowNull: false,
+            },
+        }, {
+            sequelize,
+            modelName: "UniversityInfo",
+            tableName: "tb_universityinfo",
+            charset: "utf8",
+            timestamps: false,
+        });
     }
-    static associate(db) {}
+
+    static associate(db) {
+        // db.UniversityInfo.hasMany(db.UnivBoard, { foreignKey: "UnivNo", sourceKey: "UnivNo" });
+    }
 };
