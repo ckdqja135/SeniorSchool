d32b00c919d89287ae4ce386791d677a9c435a1c
ckdqja135 <ckdqja135@naver.com>
2025-02-01 12:01:51 +0900

    refactor: 사용하지 않는 라우터 디렉토리 삭제 주석 추가

--- Backend/routes/board.router.js
+++ Backend/routes/board.router.js
@@ -2,11 +2,15 @@ const express = require('express');
 const router = express.Router();
 const boardController = require('../controller/boardController');
 
-// 라우트 정의
+// 게시판 목록
 router.get('/', boardController.getBoards);
+// 게시판 상세보기
 router.get('/detail', boardController.getBoardDetail);
+// 게시판 등록
 router.post('/insert', boardController.insertBoard);
+// 게시판 수정
 router.put('/correct', boardController.correctBoard);
+// 게시판 삭제
 router.delete('/delete', boardController.deleteBoard);
 
 module.exports = router;
