962f6fc6584fcee2e69940f737ca246ee4912024
ckdqja135 <ckdqja135@naver.com>
2025-04-19 16:55:33 +0900

    feat : router 토큰 처리 추가

--- Backend/routes/admin/report.router.js
+++ Backend/routes/admin/report.router.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const reportController = require('../../controller/admin/reportController');
+const { isAdmin, authenticateToken } = require('../../middlewares/authMiddleware');
 
 // 신고 게시판 등록
-router.post('/createReport', reportController.createReport);
+router.post('/createReport', authenticateToken, isAdmin, reportController.createReport);
 
 // 신고 게시판 리스트
-router.get('/getReports', reportController.getReports);
+router.get('/getReports', authenticateToken, isAdmin, reportController.getReports);
 
 // 신고 게시판 상세
-router.get('/getReportDetail', reportController.getReportDetail);
+router.get('/getReportDetail', authenticateToken, isAdmin, reportController.getReportDetail);
 
 // 신고 처리 상태 업데이트
-router.patch('/updateReportStatus', reportController.updateReportStatus);
+router.patch('/updateReportStatus', authenticateToken, isAdmin, reportController.updateReportStatus);
 
 // 신고 게시판 삭제
-router.delete('/deleteReportBoard', reportController.deleteReportBoard);
+router.delete('/deleteReportBoard', authenticateToken, isAdmin, reportController.deleteReportBoard);
 
 module.exports = router;
