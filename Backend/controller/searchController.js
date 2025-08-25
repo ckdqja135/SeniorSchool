23b98a2f79c4e70573741b99a397c28a00cd9bd6
ckdqja135 <ckdqja135@naver.com>
2025-08-25 20:30:04 +0900

    model : 학교 정보 테이블 칼럼 속성 수정

--- Backend/controller/searchController.js
+++ Backend/controller/searchController.js
@@ -22,6 +22,10 @@ exports.autoComplete = async (req, res) => {
 
 exports.getSchoolInfo = async (req, res) => {
     try {
+        logger.info(`[getSchoolInfo] Request received: ${JSON.stringify(req.query)}`);
+        logger.info(`[getSchoolInfo] Request URL: ${req.url}`);
+        logger.info(`[getSchoolInfo] Request path: ${req.path}`);
+        
         const { univName } = req.query;
 
         if (!univName) {
--- Backend/model/universityinfo.js
+++ Backend/model/universityinfo.js
@@ -47,7 +47,7 @@ module.exports = class UniversityInfo extends Sequelize.Model {
                     allowNull: false,
                 },
                 univLotAddr: {
-                    type: Sequelize.INTEGER,
+                    type: Sequelize.STRING(20),
                     allowNull: false,
                 },
                 univAddr: {
